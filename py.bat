c6ef2e8d73609b3d7cc3b0991e40dcfe6dcfeb43
RyougiKukoc <38523526+RyougiKukoc@users.noreply.github.com>
2026-07-12 11:38:39 +0800

    Update Python executable path and error handling

--- py.bat
+++ py.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal
 
-set "PYTHON_TO_USE=C:\green\WPy64-313110\python\python.exe"
+set "PYTHON_TO_USE=C:\WPy64-313-Vapoursynth R73\python\python.exe"
 
 :execute
+echo --------------------------------------------------------------------------------
 echo Working Directory: "%CD%"
 echo Executing: "%PYTHON_TO_USE%" %*
+echo --------------------------------------------------------------------------------
 "%PYTHON_TO_USE%" %*
+set "EXIT_CODE=%ERRORLEVEL%"
 
-PAUSE
+if not "%EXIT_CODE%"=="0" pause
+exit /b %EXIT_CODE%
